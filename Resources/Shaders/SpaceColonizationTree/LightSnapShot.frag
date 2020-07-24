2fc611e2d4f1e3bc06a53b6f62c3a4b576c21cb9
Edison Li <li2343@purdue.edu>
2020-07-24 15:41:26 -0400

    Removed resource & include folder

--- Resources/Shaders/SpaceColonizationTree/LightSnapShot.frag
+++
@@ -1,7 +0,0 @@
-layout (location = 0) out vec4 vFragColor;
-
-void main()
-{
-	float depth = gl_FragCoord.z;
-	vFragColor = vec4(depth, 0, 0, 1);
-}
--- includes/Assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/lllll/Documents/GitHub/UniEngine/UniEngine/includes/assimp-5.0.1/contrib/zlib
-# Build directory: C:/Users/lllll/Documents/GitHub/UniEngine/UniEngine/includes/Assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
