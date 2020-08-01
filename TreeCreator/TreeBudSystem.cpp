f6677aff176d3fbddb6f5d8cb38fba3b98c670fe
Edison Li <li2343@purdue.edu>
2020-08-01 19:19:52 -0400

    Check Point

---
+++ TreeCreator/TreeBudSystem.cpp
@@ -0,0 +1,18 @@
+#include "TreeBudSystem.h"
+
+void TreeCreator::TreeBudSystem::OnCreate()
+{
+}
+
+void TreeCreator::TreeBudSystem::OnDestroy()
+{
+}
+
+void TreeCreator::TreeBudSystem::Update()
+{
+
+}
+
+void TreeCreator::TreeBudSystem::FixedUpdate()
+{
+}
---
+++ TreeCreator/TreeBudSystem.h
@@ -0,0 +1,82 @@
+#pragma once
+#include "UniEngine.h"
+using namespace UniEngine;
+namespace TreeCreator {
+#pragma region Common
+    struct Mass : ComponentBase {
+        float Value;
+    };
+    struct Position : ComponentBase {
+        glm::vec3 Value;
+    };
+
+    struct Direction : ComponentBase
+    {
+        glm::vec3 Value;
+    };
+#pragma endregion
+
+#pragma region Leaf
+    struct LeafIndex : ComponentBase {
+        unsigned Value;
+    };
+#pragma endregion
+
+#pragma region Bud
+    struct Radius : ComponentBase
+    {
+        float Value;
+    };
+
+    struct BudIndex : ComponentBase {
+        unsigned Value;
+    };
+
+    struct Iteration : ComponentBase {
+        unsigned Value;
+    };
+    //Gravelius Order
+    struct Level : ComponentBase {
+        unsigned Value;
+    };
+
+    struct Phototropism : ComponentBase {
+        float Value;
+    };
+
+    //Branches tend to grow against gravity
+    struct Gravitropism : ComponentBase {
+        float Value;
+    };
+#pragma endregion
+
+#pragma region Tree
+    struct TreeIndex : ComponentBase {
+        unsigned Value;
+    };
+#pragma endregion
+
+    struct TreeBud {
+        Entity entity;
+        Entity parent;
+        std::vector<Entity> children;
+        std::vector<Entity> leaves;
+    };
+
+    struct Tree {
+        Entity _TreeEntity;
+        Entity _BudRoot;
+    };
+
+    class TreeBudSystem :
+        public SystemBase
+    {
+        Tree _Tree;
+        bool _DrawLeaves;
+    public:
+        void OnCreate();
+        void OnDestroy();
+        void Update();
+        void FixedUpdate();
+    };
+}
