4bc9d1c6dd76b1aa25ddffbb3fb90f2cdb5c776b
Edison Li <li2343@purdue.edu>
2020-08-16 17:08:17 -0400

    Parent System improvement. 10% Speed improvement

--- UniEngine/ParentSystem.cpp
+++ UniEngine/ParentSystem.cpp
@@ -3,7 +3,7 @@
 #include "UniEngine.h"
 void UniEngine::ParentSystem::OnCreate()
 {
-	_CachedParentHierarchies = std::vector<std::vector<std::pair<Entity, Entity>>>();
+	_CachedParentHierarchies = std::vector<std::pair<Entity, ChildInfo>>();
 	Enable();
 }
 
@@ -29,7 +29,6 @@ void UniEngine::ParentSystem::CalculateLTWRecursive(LocalToWorld pltw, Entity en
 		CalculateLTW(ltw, i);
 		});
 	*/
-
 	for (const auto& i : EntityManager::GetChildren(entity)) {
 		auto ltp = EntityManager::GetComponentData<LocalToParent>(i);
 		LocalToWorld ltw;
@@ -39,14 +38,36 @@ void UniEngine::ParentSystem::CalculateLTWRecursive(LocalToWorld pltw, Entity en
 	}
 }
 
-void UniEngine::ParentSystem::CollectHierarchy(std::vector<std::pair<Entity, Entity>>* container, Entity entity)
+void UniEngine::ParentSystem::CollectHierarchy(std::vector<std::pair<Entity, ChildInfo>>* container, Entity entity)
 {
 	auto children = EntityManager::GetChildren(entity);
-	for (const auto& i : children) container->push_back(std::pair<Entity, Entity>(entity, i));
-	for (int i = 0; i < container->size(); i++) {
-		Entity target = container->at(i).second;
-		for (const auto& child : EntityManager::GetChildren(target))
-			container->push_back(std::make_pair(target, child));
+	auto initialSize = container->size();
+	for (const auto& i : children) {
+		if (EntityManager::HasComponentData<LocalToWorld>(i) && EntityManager::HasComponentData<LocalToParent>(i) && EntityManager::HasComponentData<LocalToWorld>(entity)) {
+			ChildInfo info;
+			info.Child = i;
+			info.LastLTP = EntityManager::GetComponentData<LocalToParent>(i);
+			info.LastPLTW = EntityManager::GetComponentData<LocalToWorld>(entity);
+			container->push_back(std::make_pair(entity, info));
+			LocalToWorld ltw;
+			ltw.value = info.LastPLTW.value * info.LastLTP.value;
+			EntityManager::SetComponentData<LocalToWorld>(i, ltw);
+		}
+	}
+	for (int i = initialSize; i < container->size(); i++) {
+		Entity target = container->at(i).second.Child;
+		for (const auto& child : EntityManager::GetChildren(target)) {
+			if (EntityManager::HasComponentData<LocalToWorld>(child) && EntityManager::HasComponentData<LocalToParent>(child) && EntityManager::HasComponentData<LocalToWorld>(target)) {
+				ChildInfo info;
+				info.Child = child;
+				info.LastLTP = EntityManager::GetComponentData<LocalToParent>(child);
+				info.LastPLTW = EntityManager::GetComponentData<LocalToWorld>(target);
+				container->push_back(std::make_pair(target, info));
+				LocalToWorld ltw;
+				ltw.value = info.LastPLTW.value * info.LastLTP.value;
+				EntityManager::SetComponentData<LocalToWorld>(child, ltw);
+			}
+		}
 	}
 }
 
@@ -64,31 +85,51 @@ void UniEngine::ParentSystem::Update()
 	for (auto i : futures) i.wait();
 	*/
 
-	if (EntityManager::GetParentHierarchyVersion() != _CurrentStoredHierarchyVersion) {
-		for (int i = 0; i < _CachedParentHierarchies.size(); i++) {
-			_CachedParentHierarchies[i].clear();
+	if (EntityManager::GetParentHierarchyVersion() == _CurrentStoredHierarchyVersion) {
+		std::vector<std::shared_future<void>> futures;
+		auto list = &_CachedParentHierarchies;
+		auto threadSize = _ThreadPool->Size();
+		size_t capacity = _CachedParentHierarchies.size() / threadSize;
+		size_t reminder = _CachedParentHierarchies.size() % threadSize;
+		for (size_t i = 0; i < threadSize; i++) {
+			futures.push_back(_ThreadPool->Push([&list, i, capacity](int id)
+				{
+					for (size_t j = 0; j < capacity; j++) {
+						size_t index = capacity * i + j;
+						auto info = list->at(index);
+						auto pltw = EntityManager::GetComponentData<LocalToWorld>(info.first);
+						auto ltp = EntityManager::GetComponentData<LocalToParent>(info.second.Child);
+						if (info.second.LastPLTW == pltw && info.second.LastLTP == ltp) continue;
+						info.second.LastPLTW = pltw;
+						info.second.LastLTP = ltp;
+						list->at(index) = info;
+						LocalToWorld ltw;
+						ltw.value = pltw.value * ltp.value;
+						EntityManager::SetComponentData<LocalToWorld>(info.second.Child, ltw);
+					}
+				}
+			).share());
 		}
+		for (size_t i = 0; i < reminder; i++) {
+			size_t index = capacity * threadSize + i;
+			auto info = list->at(index);
+			auto pltw = EntityManager::GetComponentData<LocalToWorld>(info.first);
+			auto ltp = EntityManager::GetComponentData<LocalToParent>(info.second.Child);
+			if (info.second.LastPLTW == pltw && info.second.LastLTP == ltp) continue;
+			info.second.LastPLTW = pltw;
+			info.second.LastLTP = ltp;
+			list->at(index) = info;
+			LocalToWorld ltw;
+			ltw.value = pltw.value * ltp.value;
+			EntityManager::SetComponentData<LocalToWorld>(info.second.Child, ltw);
+		}
+		for (auto i : futures) i.wait();
+	}
+	else {
 		_CachedParentHierarchies.clear();
 		EntityManager::ForAllRootParent([this](int i, Entity rootParent) {
-			_CachedParentHierarchies.push_back(std::vector<std::pair<Entity, Entity>>());
-			CollectHierarchy(&_CachedParentHierarchies.at(i), rootParent);
+			CollectHierarchy(&_CachedParentHierarchies, rootParent);
 			});
 		_CurrentStoredHierarchyVersion = EntityManager::GetParentHierarchyVersion();
 	}
-
-	std::vector<std::shared_future<void>> futures;
-	auto lists = &_CachedParentHierarchies;
-	for (const auto& list : _CachedParentHierarchies) {
-		futures.push_back(_ThreadPool->Push([&list](int id) {
-			for (int j = 0; j < list.size(); j++) {
-				auto pltw = EntityManager::GetComponentData<LocalToWorld>(list[j].first);
-				auto ltp = EntityManager::GetComponentData<LocalToParent>(list[j].second);
-				LocalToWorld ltw;
-				ltw.value = pltw.value * ltp.value;
-				EntityManager::SetComponentData<LocalToWorld>(list[j].second, ltw);
-			}
-			}).share());
-	}
-	for (auto i : futures) i.wait();
-
 }
--- UniEngine/ParentSystem.h
+++ UniEngine/ParentSystem.h
@@ -1,13 +1,18 @@
 #pragma once
 #include "UniEngineAPI.h"
 namespace UniEngine {
+	struct ChildInfo {
+		Entity Child;
+		LocalToParent LastLTP;
+		LocalToWorld LastPLTW;
+	};
 	class UNIENGINE_API ParentSystem :
 		public SystemBase
 	{
 		size_t _CurrentStoredHierarchyVersion = INT_MAX;
-		std::vector<std::vector<std::pair<Entity, Entity>>> _CachedParentHierarchies;
+		std::vector<std::pair<Entity, ChildInfo>> _CachedParentHierarchies;
 		void CalculateLTWRecursive(LocalToWorld pltw, Entity entity);
-		void CollectHierarchy(std::vector<std::pair<Entity, Entity>>* container, Entity entity);
+		void CollectHierarchy(std::vector<std::pair<Entity, ChildInfo>>* container, Entity entity);
 	public:
 		void OnCreate();
 		void OnDestroy();
