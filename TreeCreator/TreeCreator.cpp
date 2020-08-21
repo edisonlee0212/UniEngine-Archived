ed47b0a1e7fb5403f5c2d49c8903b5d345292c50
Shirong Zheng <zheng460@purdue.edu>
2020-08-21 16:20:16 -0400

    Without any include

--- TreeCreator/TreeCreator.cpp
+++ TreeCreator/TreeCreator.cpp
@@ -1,65 +1,52 @@
 // SponzaTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-#include "UniEngine.h"
-#include "CameraControlSystem.h"
-#include "SpaceColonizationTreeSystem.h"
+//#include "UniEngine.h"
+//#include "CameraControlSystem.h"
+//#include "SpaceColonizationTreeSystem.h"
+
+//#include "TreeManager.h"
+//#include "EntityEditorSystem.h"
+
+//#include "EntityEditorSystem.h"
+#include <stdlib.h>
+#include <iostream>
 #include <WS2tcpip.h>
-#pragma comment(lib, "ws2_32.lib")
-#include "TreeManager.h"
-using namespace UniEngine;
-using namespace TreeUtilities;
-void InitGround();
+#pragma comment (lib, "ws2_32.lib")
+//using namespace UniEngine;
+//using namespace TreeUtilities;
+//void InitGround();
+//void InitSpaceColonizationTreeSystem();
 int startserver();
 int main()
 {
-	int a = startserver();
-	LightingManager::SetAmbientLight(1.0f);
+	startserver();
+	/*LightingManager::SetAmbientLight(1.0f);
 	Application::Init();
+
 	Application::SetTimeStep(0.016f);
 #pragma region Preparations
 	World* world = Application::GetWorld();
 	WorldTime* time = world->Time();
-	
-	EntityArchetype archetype = EntityManager::CreateEntityArchetype<Translation, Rotation, Scale, LocalToWorld>(Translation(), Rotation(), Scale(), LocalToWorld());
+	EntityEditorSystem* editorSystem = world->CreateSystem<EntityEditorSystem>(SystemGroup::PresentationSystemGroup);
+
+	EntityArchetype archetype = EntityManager::CreateEntityArchetype("General", Translation(), Rotation(), Scale(), LocalToWorld());
 	CameraControlSystem* ccs = world->CreateSystem<CameraControlSystem>(SystemGroup::SimulationSystemGroup);
 	ccs->Enable();
 	ccs->SetPosition(glm::vec3(0, 30, 60));
 	InitGround();
 #pragma endregion
-
 	TreeManager::Init();
+	InitSpaceColonizationTreeSystem();
 
-	auto sctSys = Application::GetWorld()->CreateSystem<SpaceColonizationTreeSystem>(SystemGroup::SimulationSystemGroup);
 
-	sctSys->ResetEnvelopeType(EnvelopeType::Box);
-	sctSys->ResetEnvelope(160, 20, 60);
-	sctSys->PushAttractionPoints(10000);
 
-	TreeColor treeColor;
-	treeColor.BudColor = glm::vec4(1, 0, 0, 1);
-	treeColor.ConnectionColor = glm::vec4(0.6f, 0.3f, 0, 1);
-	Entity tree1 = sctSys->CreateTree(0, treeColor, glm::vec3(30, 0, -30), false);
-
-	treeColor.BudColor = glm::vec4(0, 1, 0, 1);
-	treeColor.ConnectionColor = glm::vec4(0.6f, 0.3f, 0, 1);
-	Entity tree2 = sctSys->CreateTree(1, treeColor, glm::vec3(30, 0, 30));
-
-	treeColor.BudColor = glm::vec4(0, 0, 1, 1);
-	treeColor.ConnectionColor = glm::vec4(0.6f, 0.3f, 0, 1);
-	Entity tree3 = sctSys->CreateTree(2, treeColor, glm::vec3(-30, 0, -30));
-
-	treeColor.BudColor = glm::vec4(0, 1, 1, 1);
-	treeColor.ConnectionColor = glm::vec4(0.6f, 0.3f, 0, 1);
-	Entity tree4 = sctSys->CreateTree(3, treeColor, glm::vec3(-30, 0, 30));
-
-	sctSys->PushGrowIterations(100);
 	Application::Run();
-	Application::End();
+	Application::End();*/
 	return 0;
 }
 
-void InitGround() {
-	EntityArchetype archetype = EntityManager::CreateEntityArchetype<Translation, Rotation, Scale, LocalToWorld>(Translation(), Rotation(), Scale(), LocalToWorld());
+/*void InitGround() {
+	EntityArchetype archetype = EntityManager::CreateEntityArchetype("General", Translation(), Rotation(), Scale(), LocalToWorld());
 	auto entity = EntityManager::CreateEntity(archetype);
 	Translation translation = Translation();
 	translation.Value = glm::vec3(0.0f, 0.0f, 0.0f);
@@ -67,62 +54,49 @@ void InitGround() {
 	scale.Value = glm::vec3(100.0f);
 	EntityManager::SetComponentData<Translation>(entity, translation);
 	EntityManager::SetComponentData<Scale>(entity, scale);
-	/*
-	auto entity1 = EntityManager::CreateEntity(archetype);
-	translation.value = glm::vec3(-100.0f, 0.0f, 0.0f);
-	scale.value = glm::vec3(100.0f, 1.0f, 20.0f);
-	Rotation rotation;
-	rotation.value = glm::quatLookAt(glm::vec3(0, 1, 0), glm::vec3(1, 0, 0));
-	EntityManager::SetComponentData<Position>(entity1, translation);
-	EntityManager::SetComponentData<Scale>(entity1, scale);
-	EntityManager::SetComponentData<Rotation>(entity1, rotation);
 
-	auto entity2 = EntityManager::CreateEntity(archetype);
-	translation.value = glm::vec3(100.0f, 0.0f, 0.0f);
-	scale.value = glm::vec3(100.0f, 1.0f, 20.0f);
-	rotation.value = glm::quatLookAt(glm::vec3(0, 1, 0), glm::vec3(-1, 0, 0));
-
-	EntityManager::SetComponentData<Position>(entity2, translation);
-	EntityManager::SetComponentData<Scale>(entity2, scale);
-	EntityManager::SetComponentData<Rotation>(entity2, rotation);
-
-
-	auto entity3 = EntityManager::CreateEntity(archetype);
-	translation.value = glm::vec3(0.0f, 0.0f, -100.0f);
-	scale.value = glm::vec3(100.0f, 1.0f, 20.0f);
-	rotation.value = glm::quatLookAt(glm::vec3(0, 1, 0), glm::vec3(0, 0, 1));
-
-	EntityManager::SetComponentData<Position>(entity3, translation);
-	EntityManager::SetComponentData<Scale>(entity3, scale);
-	EntityManager::SetComponentData<Rotation>(entity3, rotation);
-
-	auto entity4 = EntityManager::CreateEntity(archetype);
-	translation.value = glm::vec3(0.0f, 0.0f, 100.0f);
-	scale.value = glm::vec3(100.0f, 1.0f, 20.0f);
-	rotation.value = glm::quatLookAt(glm::vec3(0, 1, 0), glm::vec3(0, 0, -1));
-
-	EntityManager::SetComponentData<Position>(entity4, translation);
-	EntityManager::SetComponentData<Scale>(entity4, scale);
-	EntityManager::SetComponentData<Rotation>(entity4, rotation);
-	*/
 
 	auto mat = new Material();
 	mat->Programs()->push_back(Default::GLPrograms::StandardProgram);
 	auto texture = new Texture2D(TextureType::DIFFUSE);
-	texture->LoadTexture(FileIO::GetPath("Textures/white.png"), "");
+	texture->LoadTexture(FileIO::GetPath("Textures/treesurface.jpg"), "");
 	mat->Textures2Ds()->push_back(texture);
 	mat->SetMaterialProperty("material.shininess", 32.0f);
 	MeshMaterialComponent* meshMaterial = new MeshMaterialComponent();
 	meshMaterial->_Mesh = Default::Primitives::Quad;
 	meshMaterial->_Material = mat;
 	EntityManager::SetSharedComponent<MeshMaterialComponent>(entity, meshMaterial);
-	/*
-	EntityManager::SetSharedComponent<MeshMaterialComponent>(entity1, meshMaterial);
-	EntityManager::SetSharedComponent<MeshMaterialComponent>(entity2, meshMaterial);
-	EntityManager::SetSharedComponent<MeshMaterialComponent>(entity3, meshMaterial);
-	EntityManager::SetSharedComponent<MeshMaterialComponent>(entity4, meshMaterial);*/
-}
 
+}*/
+
+/*void InitSpaceColonizationTreeSystem()
+{
+	auto sctSys = Application::GetWorld()->CreateSystem<SpaceColonizationTreeSystem>(SystemGroup::SimulationSystemGroup);
+	sctSys->ResetEnvelopeType(EnvelopeType::Box);
+	sctSys->ResetEnvelope(160, 20, 60);
+	sctSys->PushAttractionPoints(10000);
+
+	TreeColor treeColor;
+	treeColor.Color = glm::vec4(1, 1, 1, 1);
+	treeColor.BudColor = glm::vec4(1, 0, 0, 1);
+	treeColor.ConnectionColor = glm::vec4(0.6f, 0.3f, 0, 1);
+	treeColor.LeafColor = glm::vec4(0, 1, 0, 1);
+	Entity tree1 = sctSys->CreateTree(treeColor, glm::vec3(30, 0, -30));
+
+	treeColor.BudColor = glm::vec4(0, 1, 0, 1);
+	treeColor.ConnectionColor = glm::vec4(0.6f, 0.3f, 0, 1);
+	Entity tree2 = sctSys->CreateTree(treeColor, glm::vec3(30, 0, 30));
+
+	treeColor.BudColor = glm::vec4(0, 0, 1, 1);
+	treeColor.ConnectionColor = glm::vec4(0.6f, 0.3f, 0, 1);
+	Entity tree3 = sctSys->CreateTree(treeColor, glm::vec3(-30, 0, -30));
+
+	treeColor.BudColor = glm::vec4(0, 1, 1, 1);
+	treeColor.ConnectionColor = glm::vec4(0.6f, 0.3f, 0, 1);
+	Entity tree4 = sctSys->CreateTree(treeColor, glm::vec3(-30, 0, 30));
+
+	sctSys->PushGrowAllTreesIterations(100);
+}*/
 
 
 int startserver() {
@@ -130,14 +104,14 @@ int startserver() {
 	// Initialze winsock
 	WSADATA wsData;
 	WORD ver = MAKEWORD(2, 2);
-
+	cout << "Sever Start" << endl;
 	int wsOk = WSAStartup(ver, &wsData);
 	if (wsOk != 0)
 	{
 		cerr << "Can't Initialize winsock! Quitting" << endl;
 		return -1;
 	}
-
+	//int64_t s = socket(address->addr.sa_family, SOCK_DGRAM, IPPROTO_UDP);
 	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
 	if (listening == INVALID_SOCKET)
 	{
@@ -147,59 +121,74 @@ int startserver() {
 
 	sockaddr_in hint;
 	hint.sin_family = AF_INET;
-	hint.sin_port = htons(54000);
+	hint.sin_port = htons(57000);
 	hint.sin_addr.S_un.S_addr = INADDR_ANY; // Could also use inet_pton .... 
+	int off = 0;
+	cout << "Set Socket opt" << endl;
+	int result = setsockopt(listening, IPPROTO_IPV6, IPV6_V6ONLY, (char*)&off, sizeof(off));
+	if (result != 0)
+	{
+		//*errorcode = native_get_last_error();
+		closesocket(listening);
+		return -1;
+	}
+	cout << "Before Binding" << endl;
 	bind(listening, (sockaddr*)&hint, sizeof(hint));
 	listen(listening, SOMAXCONN);
 
 
 	sockaddr_in client;
 	int clientSize = sizeof(client);
-	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
-	char host[NI_MAXHOST];		
-	char service[NI_MAXSERV];	
+	while (true) {
+		SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+		char host[NI_MAXHOST];
+		char service[NI_MAXSERV];
 
-	ZeroMemory(host, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
-	ZeroMemory(service, NI_MAXSERV);
+		ZeroMemory(host, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
+		ZeroMemory(service, NI_MAXSERV);
 
-	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
-	{
-		cout << host << " connected on port " << service << endl;
-	}
-	else
-	{
-		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-		cout << host << " connected on port " <<
-			ntohs(client.sin_port) << endl;
-	}
-
-	closesocket(listening);
-
-	char buf[4096];
-
-	while (true)
-	{
-		ZeroMemory(buf, 4096);
-		int bytesReceived = recv(clientSocket, buf, 4096, 0);
-		if (bytesReceived == SOCKET_ERROR)
+		if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
 		{
-			cerr << "Error in recv(). Quitting" << endl;
-			break;
+			cout << host << " connected on port " << service << endl;
+			cout << "this one " << endl;
+		}
+		else
+		{
+			inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+			cout << ntohs(hint.sin_port) << endl;
+			cout << "second one " << endl;
+			cout << host << " connected on port " <<
+				ntohs(client.sin_port) << endl;
 		}
 
-		if (bytesReceived == 0)
+		closesocket(listening);
+
+		char buf[4096];
+
+		while (true)
 		{
-			cout << "Client disconnected " << endl;
-			break;
+			ZeroMemory(buf, 4096);
+			int bytesReceived = recv(clientSocket, buf, 4096, 0);
+			if (bytesReceived == SOCKET_ERROR)
+			{
+				cerr << "Error in recv(). Quitting" << endl;
+				break;
+			}
+
+			if (bytesReceived == 0)
+			{
+				cout << "Client disconnected " << endl;
+				break;
+			}
+
+			cout << string(buf, 0, bytesReceived) << endl;
+			send(clientSocket, buf, bytesReceived + 1, 0);
+
 		}
 
-		cout << string(buf, 0, bytesReceived) << endl;
-		send(clientSocket, buf, bytesReceived + 1, 0);
+		closesocket(clientSocket);
+		WSACleanup();
 
+		system("pause");
 	}
-
-	closesocket(clientSocket);
-	WSACleanup();
-
-	system("pause");
 }
