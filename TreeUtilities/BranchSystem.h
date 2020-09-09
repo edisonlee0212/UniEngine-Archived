bc18ec554ab4322cabd05796c34603a2ddb50518
Edison Li <li2343@purdue.edu>
2020-09-09 18:53:03 -0400

    Removed projects.

--- TreeUtilities/BranchSystem.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "TreeUtilitiesAPI.h"
-using namespace UniEngine;
-namespace TreeUtilities {
-    class BranchSystem :
-        public SystemBase
-    {
-    };
-}
--- TreeUtilities/TreeUtilitiesAPI.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "pch.h"
-#include "UniEngine.h"
-#ifdef TREEUTILITIES_EXPORTS
-#define TREEUTILITIES_API __declspec(dllexport)
-#else
-#define TREEUTILITIES_API __declspec(dllimport)
-#endif
