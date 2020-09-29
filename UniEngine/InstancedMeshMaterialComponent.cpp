d9f0f300a078eae72992903bc54ee6972307717f
Edison Li <li2343@purdue.edu>
2020-09-29 19:59:41 -0400

    Update InstancedMeshMaterialComponent.cpp

--- UniEngine/InstancedMeshMaterialComponent.cpp
+++ UniEngine/InstancedMeshMaterialComponent.cpp
@@ -3,9 +3,12 @@
 
 size_t UniEngine::InstancedMeshMaterialComponent::GetHashCode()
 {
-	return ((size_t)this);
+	return (size_t)this;
 }
 
 void UniEngine::InstancedMeshMaterialComponent::OnGui()
 {
+	ImGui::Checkbox("Cast shadow", &CastShadow);
+	ImGui::Checkbox("Receive shadow", &ReceiveShadow);
+	ImGui::Text(("Instance count: " + std::to_string(Matrices.size())).c_str());
 }
